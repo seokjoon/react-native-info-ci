96be189a53933c9093ef2147adf4ae0ce42bb54d
seokjoon <seokjoon@gmail.com>
2022-09-29 23:13:00 +0900

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import TestAppCom from './src/com/Test/App/TestAppCom';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TestAppCom);
